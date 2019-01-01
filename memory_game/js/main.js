37b10933e419357d991d706d41f656b8fa846505
Lucas Lombardo <Lucasl@bu.edu>
2019-01-01 15:26:43 -0500

    adds dom manipulation

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const cards = [
   { rank: "queen", suit: "hearts", cardImg: "images/queen-of-hearts.png" },
   { rank: "queen", suit: "diamonds", cardImg: "images/queen-of-diamonds.png" },
@@ -7,7 +9,8 @@ const cards = [
 
 const cardsInPlay = [];
 
-function checkForMatch() {
+function checkForMatch(cardId, cardClicked) {
+  cardClicked.setAttribute("src", cards[cardId].cardImg);
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match!");
@@ -17,14 +20,24 @@ function checkForMatch() {
   }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+  const cardId = this.getAttribute("data-id");
   console.log("user flipped " + cards[cardId].rank);
   console.log(
     `flipped suit = ${cards[cardId].suit} and img = ${cards[cardId].cardImg}`
   );
   cardsInPlay.push(cards[cardId].rank);
-  checkForMatch();
+  checkForMatch(cardId, this);
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.querySelector("#game-board").appendChild(cardElement);
+  }
+}
+
+createBoard();
